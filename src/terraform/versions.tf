b1697f2767f1399771d3bf626562ef8ca580f198
VOMARTE <daniel.martens@mercedes-benz.io>
2022-03-02 13:49:54 +0100

    add terraform provider hetzner cloud

--- src/terraform/versions.tf
+++ src/terraform/versions.tf
@@ -1,3 +1,15 @@
 terraform {
     required_version = ">= 1.1.6"
+
+    required_providers {
+        hcloud = {
+        source = "hetznercloud/hcloud"
+        version = "~> 1.33.1"
+        }
+    }
+}
+
+# Configure the Hetzner Cloud Provider
+provider "hcloud" {
+  token = var.hcloud_token
 }
