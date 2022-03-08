3e7d0cafcd9a39a6f7ce4be1bb5d41082be1e82c
VOMARTE <daniel.martens@mercedes-benz.io>
2022-03-08 13:29:22 +0100

    ssh key generation

---
+++ src/terraform/main.tf
@@ -0,0 +1,30 @@
+##### SSH Key mgmt #####
+# Generate SSH key pair locally
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+  # Exported attributes
+  # - public_key_openssh
+  # - private_key_pem
+}
+
+# Store public SSH key into file for reference purposes
+resource "local_file" "ssh_public_key_file" {
+  content         = tls_private_key.ssh.public_key_openssh
+  filename        = var.ssh_public_key_file
+  file_permission = "0755"
+}
+
+# Store private SSH key into file
+resource "local_file" "ssh_private_key_file" {
+  sensitive_content = tls_private_key.ssh.private_key_pem
+  filename          = var.ssh_private_key_file
+  file_permission   = "0600"
+}
+
+# Register public SSH key in hetzner
+resource "hcloud_ssh_key" "kubeone" {
+  name       = "kubeone-${var.cluster_name}"
+  public_key = tls_private_key.ssh.public_key_openssh
+}
+########################
--- src/terraform/variables.tf
+++ src/terraform/variables.tf
@@ -1,3 +1,5 @@
+
+# Access to providers
 variable "hcloud_token" {
   description = "Hetzner Cloud access token"
   type = string
@@ -16,6 +18,7 @@ variable "godaddy_secret" {
   sensitive = true
 }
 
+# Global settings
 variable "tld" {
   description = "Top level domain for the cluster"
   type = string
@@ -30,4 +33,39 @@ variable "cluster_name" {
     condition     = can(regex("^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$", var.cluster_name))
     error_message = "Value of cluster_name should be lowercase and can only contain alphanumeric characters and hyphens(-)."
   }
+}
+
+# SSH key settings
+variable "ssh_public_key_file" {
+  description = "SSH public key file"
+  type        = string
+  default     = "../../data/id_rsa.pub"
+}
+
+variable "ssh_private_key_file" {
+  # Only used for terraform output as kubeone input
+  description = "SSH private key file used to access instances"
+  type        = string
+  default     = "../../data/id_rsa"
+}
+
+variable "ssh_username" {
+  # Only used for terraform output as kubeone input
+  description = "SSH user"
+  type        = string
+  default     = "root"
+}
+
+variable "ssh_port" {
+  # Only used for terraform output as kubeone input
+  description = "SSH port to be used to provision instances"
+  type        = number
+  default     = 22
+}
+
+variable "ssh_agent_socket" {
+  # Only used for terraform output as kubeone input
+  description = "SSH Agent socket, default to grab from $SSH_AUTH_SOCK"
+  type        = string
+  default     = "env:SSH_AUTH_SOCK"
 }
